d6c63251b1a85481d83a49997e4ed1c3af3ec2fc
C07-T\강사 <knyatom@daum.net>
2023-05-18 13:05:33 +0900

    자바스크립트 오전 수업 끝

---
+++ js/0518-1.js
@@ -0,0 +1,54 @@
+// for in 반복문
+// window
+for (var i in window) {
+ console.log(`${i} : ${window[i]}`); 
+}
+
+var user = [{
+ "userId": 1,
+ "id": 1,
+ "title": "welcome occaecati excepturi optio reprehenderit",
+ "body": "quia et suscipit\nsusc eveniet architecto"
+},
+{
+ "userId": 1,
+ "id": 2,
+ "title": "qui est esse",
+ "body": "est rerum tempore vitaqui neque nisi nulla"
+},
+{
+ "userId": 1,
+ "id": 3,
+ "title": "ea molestxercitationesit aut",
+ "body": "et iusto soloribus vele et velit aut"
+}];
+console.log(user[0].title);
+
+for(var i=0; i<user.length; i++){
+  document.write("title :" + user[i].title);
+  document.write("<br>")
+}
+
+for(var i=0; i<user.length; i++){
+ document.write(user[i].id +" : " +user[i].title);
+ document.write("<br>")
+}
+
+for(var i=0; i<user.length; i++){
+ document.write(user[i].body);
+ document.write("<br>")
+}
+
+for(var i in user){
+ document.write(user[i].title);
+ console.log(user[i]);
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(json => console.log(json));
+
