f495ed84043c835ce544089e52c9adc34688a143
C07-T\강사 <knyatom@daum.net>
2023-05-23 17:09:56 +0900

    am 차트에 데이터 바인딩하기

--- js/523-2.js
+++ js/523-2.js
@@ -10,18 +10,40 @@ function search() {
   .then(data => {
    console.log(Array.isArray(data));  // true
    var output = "<table border='1'>";
-   output += "<tr><th>순번</th><th>이름</th><th>전화번호</th><th>이메일</th></tr>"
+   output += "<tr><th>순번</th><th>이름</th><th>전화번호</th><th>이메일</th><th>삭제</tr>"
    data.forEach((ele, i) => {
     output += "<tr>"
     output += "<td>" + "<a href='#' onclick=detail(" + (i + 1) + ")>" + (i + 1) + "</a>" + "</td>"
     output += "<td>" + ele.username + "</td>"
     output += "<td>" + ele.phone + "</td>"
     output += "<td>" + ele.email + "</td>"
+    output += "<td> <a onclick=deletes("+(i + 1)+")> 삭제 </a>+ </td>"
     output += "</tr>"
    })
    output += "</table>"
    content.innerHTML = output;
   })
+}
+
+function deletes(id){
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then(response => response.json())
+ .then(data => {
+  console.log(Array.isArray(data));  // true
+  var output = "<table border='1'>";
+  output += "<tr><th>순번</th><th>이름</th><th>전화번호</th><th>이메일</th><th>삭제</tr>"
+  data.filter((ele, i) => {   
+   output += "<tr>"
+   output += "<td>" + "<a href='#' onclick=detail(" + (i + 1) + ")>" + (i + 1) + "</a>" + "</td>"
+   output += "<td>" + ele.username + "</td>"
+   output += "<td>" + ele.phone + "</td>"
+   output += "<td>" + ele.email + "</td>"
+   output += "<td> <a onclick=deletes("+(i + 1)+")> 삭제 </a>+ </td>"
+   output += "</tr>"
+  })
+  output += "</table>"
+  content.innerHTML = output;
+ })
 }
 
 // 순번 사용자이름, 전화번호, 이메일
@@ -56,7 +78,7 @@ function search2() {
    data.forEach((ele, i) => {
     if (i <= 10) {
      output += "<tr>"
-     output += "<td>" + "<a href='javascript:void(0)' onclick=detail2(" + (i + 1) + ")>" + (i + 1) + "</a>" + "</td>"
+     output += "<td>" + "<a href='#' onclick=detail2(" + (i + 1) + ")>" + (i + 1) + "</a>" + "</td>"
      output += "<td>" + ele.id + "</td>"
      output += "<td>" + ele.title + "</td>"
      output += "<td>" + `${ele.completed === true ? "작업완료" : "아직미완성"}`
