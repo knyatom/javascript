0731ad12472e61de0dfe0ef10e383bca48ad47c0
C07-T\강사 <knyatom@daum.net>
2023-06-02 17:51:45 +0900

    제이쿼리 실습1일차

--- js/jq01.js
+++ js/jq01.js
@@ -62,8 +62,17 @@ $('li:nth-child(2n)').css('color',"red");
 $('li:nth-of-type(2n+1)').css('color',"blue");
 $('li:contains("menu5")').css('border-radius',"30px");
 
+// li:has("strong")  li태그중에 strong태그가 있는 객체선택
+// $('li').closest('div')   li태그에서 가장가까운 div태그 선택
+// $('a,span')  복합선택자 a태그, span태그 선택
+// $('a','li') li태그에서 그 하위태그 a를 선택해라
+// $('li').find('a')    li태그에서 그 하위태그 a를 선택해라
+// $('li a')
 
-
+$('li:has("span")').css('width','200px'); 
+$('li').closest('ul').css('padding','20px'); 
+$('a','li').css('font-weight','bold');
+$('li').find('a').css('font-style','italic'); 
 
 
 
